fc5c865200fb581a63122af767776f6e9306d1c1
Sandra Nihama <sandracomp.obi@gmail.com>
2021-12-22 08:56:51 -0300

    removing comments

--- autocorrectors/exospart2.py
+++ autocorrectors/exospart2.py
@@ -1,11 +1,6 @@
 import hashlib
 
 
-# user_id = salt + str(user_id)
-# user_id = user_id.encode(encoding)
-# user_id = sha256(user_id).digest()
-# user_id = base64.b64encode(user_id).decode(encoding)
-
 def part2exo2(rows, columns, nbresults):
     right_hashed_answer = 'caa27da5d078708b1596b44cd98a4d4aac95b0b2fbdfc98b2ce762c8024d0ed3'
     received_answer = rows + columns + nbresults
@@ -17,12 +12,7 @@ def part2exo2(rows, columns, nbresults):
     else:
         print("Oups, try again! Check your calculations on the dataframe size and your answer on the SQL task")
 
-# def part2exo4(answer):
-    # rightanswer = 43.90552325581396
-    # if answer > rightanswer - 0.1 and answer < rightanswer + 0.1:
-    #     print("Good job!")
-    # else:
-    #     print("Oups, try again!")
+
 def part2exo4(answerculmen, answersql):
     right_hashed_answer = '7e9d8f203a2b39b6162dc4bea6777c5b467a4aa0d09450b8b938772b6c2b0a71'
     received_answer = answerculmen + answersql
